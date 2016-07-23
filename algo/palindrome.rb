45f1655504d39e20e150f0247f28a79e05831acc
Andy Zhao <andyzhao.zhao@gmail.com>
2016-07-23 11:09:54 -0400

    finished palindrome.rb, renamed splitting_arrays

--- algo/palindrome.rb
+++ algo/palindrome.rb
@@ -1,7 +1,25 @@
-# Complete the method called palindrome? which should accept a string as a parameter and return a boolean indicated whether the string is a palindrome. A palindrome is a word that reads the same both forwards and backwards. Examples: eye, madam, racecar
+# Complete the method called palindrome? which should accept a string as a
+# parameter and return a boolean indicated whether the string is a palindrome.
+# A palindrome is a word that reads the same both forwards and backwards.
+# Examples: eye, madam, racecar
 
 def palindrome?(string)
-
+  array = string.split("")
+  a = 0
+  z = array.length - 1
+  all_true = false
+  true_count = 0
+  array.length.times do
+    if array[a] == array[z]
+      true_count += 1
+      a += 1
+      z -= 1
+    end
+    if true_count == array.length
+      all_true = true
+    end
+  end
+  all_true
 end
 
 # Driver code - don't touch anything below this line.
